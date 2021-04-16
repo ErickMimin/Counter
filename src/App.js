829375fb07fcd37ba311ac9110d41d651f51fe24
ErickMimin <Eri_mimo@hotmail.com>
2021-04-16 02:23:26 -0500

    [Initial] Empezamos y terminaos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const obtenerLocalStorage = () => {
+  const tareasStr = localStorage.getItem('count');
+  return JSON.parse(tareasStr) || {jump: 1, count: 0}
+}
+
+const actualizarLocalStorage = (data) => {
+  localStorage.setItem('count', JSON.stringify(data))
+}
+
 function App() {
+  const {jump: j, count: c} = obtenerLocalStorage();
+  const [jump, setJump] = useState(j);
+  const [count, setCount] = useState(c);
+
+  useEffect(()=>{
+    actualizarLocalStorage({jump, count})
+  }, [jump, count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <h1>{count}</h1><br/>
+        <button onClick={()=>{setCount(count - jump)}} className="primary">Less</button>
+        <button onClick={()=>{setCount(count + jump)}} className="primary">More</button><br/>
+        <button onClick={()=>{setJump(jump - 1)}} className="secondary">-</button>
+        <span>{jump}</span>
+        <button onClick={()=>{setJump(jump + 1)}} className="secondary">+</button>
+
+      </div>
     </div>
   );
 }
